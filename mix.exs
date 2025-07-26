a07e6cdd33e804673b091b290c457867a2cb1c57
Alexis Ruccius <a.ruccius@posteo.de>
2025-07-26 17:43:07 +0200

    Add description and hex docs

--- mix.exs
+++ mix.exs
@@ -5,9 +5,17 @@ defmodule Led.MixProject do
     [
       app: :led,
       version: "0.1.0",
+      description:
+        "Blink LEDs or relays via GPIO with ease. Features artful gimmicks for creative setups.",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # ExDocs
+      source_url: "https://github.com/alexisruccius/led",
+      homepage_url: "https://github.com/alexisruccius/led",
+      docs: docs(),
+      # Hex
+      package: package()
     ]
   end
 
@@ -22,9 +30,29 @@ defmodule Led.MixProject do
   defp deps do
     [
       {:circuits_gpio, "~> 2.1"},
-      {:circuits_sim, "~> 0.1.2"}
+      {:circuits_sim, "~> 0.1.2"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  # ExDocs
+  defp docs do
+    [
+      # The main page in the docs
+      main: "LED",
+      logo: "assets/LED-logo.jpg",
+      extras: ["README.md", "CHANGELOG.md"]
+    ]
+  end
+
+  # Hex package
+  defp package do
+    [
+      maintainers: ["Alexis Ruccius"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/alexisruccius/led"}
     ]
   end
 end
